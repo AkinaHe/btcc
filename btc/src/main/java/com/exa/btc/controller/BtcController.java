c259ef84ef1d99ac3ff07fea1e5f5b2b7174d244
AkinaHe <40930394+AkinaHe@users.noreply.github.com>
2018-12-16 10:33:49 +0800

    Update BtcController.java
    a

--- btc/src/main/java/com/exa/btc/controller/BtcController.java
+++ btc/src/main/java/com/exa/btc/controller/BtcController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class BtcController {
-    @RequestMapping("/bb")
+    @RequestMapping("/aa")
     @ResponseBody
     public String he(){
         return  "btc";
